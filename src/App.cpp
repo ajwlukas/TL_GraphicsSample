42e57bc52e8c6c79751f08af87851e47ab5c917d
ajwlukas <ajwlukas@gmail.com>
2022-11-10 11:53:52 +0900

    Texture, uv를 이용한 example 작성
    작성함

--- src/App.cpp
+++ src/App.cpp
@@ -3,14 +3,16 @@
 #include "Example\00_Ex_Triagle\Ex_Triangle.h"
 #include "Example\01_Ex_TriangleColor\Ex_TriangleColor.h"
 #include "Example\02_Ex_BoxCamera\Ex_BoxCamera.h"
-#include "Example\03_Ex_Light\Ex_Light.h"
+#include "Example\03_Ex_Texture\Ex_Texture.h"
+#include "Example\04_Ex_Light\Ex_Light.h"
 
 App::App()
 {
 	//example = new Ex_Triangle();
 	//example = new Ex_TriangleColor();
 	//example = new Ex_BoxCamera();
-	example = new Ex_Light();
+	example = new Ex_Texture();
+	//example = new Ex_Light();
 
 
 	example->Init();
